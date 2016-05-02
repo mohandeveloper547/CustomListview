565f4d9d46cfc114ab706b209a902947dacd6d14
shamsundhar <shyam.kdp@gmail.com>
2016-05-02 16:06:52 +0530

    Modified the app name
    modified the app and package name

--- CustomListView/app/src/main/java/sample/mycustomlistview/common/URLConstants.java
+++
@@ -1,8 +0,0 @@
-package sample.mycustomlistview.common;
-
-/**
- * Created by Shyam Yammanuru on 4/21/2016.
- */
-public class URLConstants {
-    public final static String JSON_URL = "https://dl.dropboxusercontent.com/u/746330/facts.json";
-}
